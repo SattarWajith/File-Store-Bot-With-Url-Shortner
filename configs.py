f6e49f5970208251e0d9ea06eee7f8a55bdc602e
SattarWajith <96818272+SattarWajith@users.noreply.github.com>
2025-03-16 17:26:01 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,7 +10,7 @@ class Config(object):
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "1ffdb6e5bc9773731be1440d0dc991a106006f2a")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "7893911398"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002492775782")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
